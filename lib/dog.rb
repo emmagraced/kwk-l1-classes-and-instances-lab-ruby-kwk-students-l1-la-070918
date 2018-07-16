431eb5e8c9d9d5efd8d6a57b04e6917b79ffd539
Emma Davis <emmadavis01@gmail.com>
2018-07-16 18:30:51 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,11 @@
-# dog.rb
+class Dog 
+  attr_accessor :fido,:snoopy,:lassie 
+  
+  def initialize(fido, snoopy, lassie)
+  
+  @fido = fido 
+    @snoopy = snoopy
+    @lassie = lassie
+end
+end
+ puts Dog
